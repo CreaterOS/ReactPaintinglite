6acff7ce23cec2ef359372a5ffbaf288ca5c6426
CreaterOS <2639563508@qq.com>
2021-05-13 15:20:59 +0800

    feat: 引入公共头文件
    # - type:
        feat(新特性),
        fix(修改问题),
        docs(文档修改),
        style(代码格式修改, 注意不是 css 修改),
        refactor(代码重构),
        test(测试),
        chore(其他修改, 比如构建流程, 依赖管理)
    # - scope: (可以为空)
        影响的的范围
        all ：表示影响面大 ，如修改了网络框架 会对真个程序产生影响
        loation： 表示影响小，某个小小的功能
        module：表示会影响某个模块 如登录模块、首页模块 、用户管理模块等等
    # - subject
        提交描述
    # - body
        具体的修改信息 应该尽量详细
    # - footer
        放置写备注啥的，如果是 bug ，可以把bug id放入

---
+++ Paintinglite/Paintinglite/Paintinglite/HeaderFiles.h
@@ -0,0 +1,14 @@
+//
+//  HeaderFiles.h
+//  Paintinglite
+//
+//  Created by 纽扣软件 on 2021/5/13.
+//  Copyright © 2021 Bryant Reyn. All rights reserved.
+//
+
+#ifndef HeaderFiles_h
+#define HeaderFiles_h
+
+#import "PaintingliteSessionManager.h"
+
+#endif /* HeaderFiles_h */
